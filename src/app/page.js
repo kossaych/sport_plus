fdc4035bd27cc140916ad85949f4aaf7dd988570
kossaych <cheminguikossay@gmail.com>
2024-01-20 21:20:17 +0100

    deploy

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,8 @@ import Courses from "@/components/Home components/courses";
 
 import { useEffect, useState } from "react";
  export default function Home() {
-  if (typeof window !== 'undefined') {
-    const token  = localStorage.getItem('token')  
-} 
-
-
+    const token = localStorage.getItem('token') 
+    
     if (token != '') {
     const [user,setUser] = useState({})
       
