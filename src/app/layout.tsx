88bd1632015db61e58e05f4de124a275cc4ddb50
kossaych <cheminguikossay@gmail.com>
2024-01-21 13:19:27 +0100

    deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,22 +26,13 @@ export default function RootLayout({
   
     
 
-    const [token,setToken] = useState('')
-
-     if (typeof localStorage !== 'undefined') { 
-
-          if ( localStorage.getItem('token') != null ){
-                setToken(localStorage.getItem('token'))
-          } 
-      
-    }  
-
+ 
   
    return (
     <html lang="en">
       <body className={inter.className}>
         <div className="bg-gray-100 ">
-          {token != ''   ?   <HeaderLogedIn></HeaderLogedIn> :  <HeaderLogedOut></HeaderLogedOut>}
+              <HeaderLogedIn></HeaderLogedIn>   <HeaderLogedOut></HeaderLogedOut>
              {children}
           <Footer></Footer>
         </div>
