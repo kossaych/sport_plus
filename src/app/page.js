2ee2e668f1d16518eaffa8367ad89a8bc98eb523
kossaych <cheminguikossay@gmail.com>
2024-01-22 21:01:06 +0100

    deploy

--- src/app/page.js
+++ src/app/page.js
@@ -6,43 +6,39 @@ import Courses from "@/components/Home components/courses";
 
 import { useEffect, useState } from "react";
  export default function Home() { 
-  
+
+
+
+
+  const [user,setUser] = useState({})
+      
+  useEffect(()=>{
+
+    fetch("http://192.168.1.111:8000/content/api/profile/", {
+      method: "get",
+      headers: {
+        'Authorization': 'token ' + "9a6c94ca1af10d19649d6c6b99c0d6b5baf7e504",
+        "Content-Type": "application/json",
+      },
+      
+    })
+    .then((response) => {
+          if (response.status === 200) {
+          return response.json();
+        }  
+      }) 
+      .then((data) => { 
+            if (data) {
+              setUser(data)
+          }
+      }) 
+  },[])
+
     if (typeof window !== "undefined") {
     
           if (localStorage.getItem('token') ) {  
 
-
-
-
-            const [user,setUser] = useState({})
-      
-            useEffect(()=>{
-          
-              fetch("http://192.168.1.111:8000/content/api/profile/", {
-                method: "get",
-                headers: {
-                  'Authorization': 'token ' + JSON.parse(localStorage.getItem('token')),
-                  "Content-Type": "application/json",
-                },
-               
-              })
-              .then((response) => {
-                   if (response.status === 200) {
-                    return response.json();
-                  }  
-                }) 
-                .then((data) => { 
-                     if (data) {
-                        setUser(data)
-                    }
-                }) 
-            },[])
-
-
-
-
-
-
+ 
           return ( 
             
             <div classNameName="bg-white"> 
