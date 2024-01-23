97e780bbab1a7091c7f80c55bd681b2036683919
kossaych <cheminguikossay@gmail.com>
2024-01-23 12:53:52 +0100

    deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,20 @@
-'use client'
-import Lives from "@/components/Home components/lives";
-import Offer from "@/components/Home components/offer";
-import Subjects from "@/components/Home components/subjects";
-import Courses from "@/components/Home components/courses"; 
-import HomeLogedIn from "@/components/Home components/home_loged_in";  
-import { useEffect, useState } from "react";
- export default function Home() { 
+'use client' 
+import HomeLogedIn from "@/components/Home components/home_loged_in";   
+export default function Home() { 
   
-  if (typeof window !== "undefined") {
-    
-          if (localStorage.getItem('token') ) {  
-          return ( 
-            <HomeLogedIn></HomeLogedIn>
-            
-           
-          )}else{
+  if (typeof window !== "undefined") { 
+        if (true ) {  
             return ( 
-            <>
-              you are not loged in
-            </>
+                  <HomeLogedIn></HomeLogedIn> 
+              )
+        }
+        else{
+            return ( 
+                <>
+                  you are not loged in
+                </>
             )
-          }
+        }
   }
  
 
